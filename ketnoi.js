3aa5ef0b66fe2ec5eff1717b337e32f7391097d8
thunghiemphanmem <hosonhatruong.c1ht@elc.vn>
2026-07-16 23:12:31 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -7,7 +7,7 @@ const KET_NOI = {
   
   // Thông tin tổ chức và định danh hiển thị
   TEN_TO_CHUC: 'TRƯỜNG TH VÀ THCS HỢP THÀNH',
-  TIEU_DE_PHAN_MEM: 'SỔ HỘI HỌP THÔNG MINH',
+  TIEU_DE_PHAN_MEM: 'SỔ HỌP THÔNG MINH',
   LOGO_TRUONG: 'https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png', // Thay URL logo thực tế của trường
   
   // Cán bộ thao tác mặc định (hoặc có thể lấy từ hệ thống đăng nhập riêng của bạn nếu có)
