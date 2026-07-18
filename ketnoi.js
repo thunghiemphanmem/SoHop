8e81139cb603c975f41ceb09e1feb000606a15a2
thunghiemphanmem <hosonhatruong.c1ht@elc.vn>
2026-07-18 12:16:33 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -5,11 +5,14 @@ const KET_NOI = {
   // Đường dẫn Web App của Google Apps Script (Lấy sau khi Deploy GAS)
   URL_API_BACKEND: 'https://script.google.com/macros/s/AKfycbw6JYOuithvUH64LxH2pc4d54BOSqdeEtquKCnCCxzzDhJIRoMZhqgaU9r7xcAmzQ5y/exec',
   
+  // Khai báo Google Client ID dùng cho đăng nhập
+  CLIENT_ID: '95180316485-a577mlt4a1ve3u52pd0t4rkm6bnljuns.apps.googleusercontent.com',
+  
   // Thông tin tổ chức và định danh hiển thị
   TEN_TO_CHUC: 'TRƯỜNG TH VÀ THCS HỢP THÀNH',
   TIEU_DE_PHAN_MEM: 'SỔ HỌP THÔNG MINH',
-  LOGO_TRUONG: 'https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png', // Thay URL logo thực tế của trường
+  LOGO_TRUONG: 'https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png', 
   
-  // Cán bộ thao tác mặc định (hoặc có thể lấy từ hệ thống đăng nhập riêng của bạn nếu có)
+  // Cán bộ thao tác mặc định
   CAN_BO_THAO_TAC: 'Cán bộ Hành chính'
 };
